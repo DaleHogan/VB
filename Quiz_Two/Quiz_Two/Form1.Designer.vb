cf235dc078f0c1672d3b9f88f406fe12323e5743
Dale Hogan <t_bone_bradley@hotmail.com>
2014-04-12 13:10:36 +1000

    Find oldest person from a listbox collection
    Simple Program to detect oldest person from a listbox

---
+++ Quiz_Two/Quiz_Two/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.EnterAgeButton = New System.Windows.Forms.Button()
+        Me.AgeListBox = New System.Windows.Forms.ListBox()
+        Me.AgeTextBox = New System.Windows.Forms.TextBox()
+        Me.EnterAgeLabel = New System.Windows.Forms.Label()
+        Me.AgeValidatorLabel = New System.Windows.Forms.Label()
+        Me.OldMemberButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'EnterAgeButton
+        '
+        Me.EnterAgeButton.Location = New System.Drawing.Point(318, 29)
+        Me.EnterAgeButton.Name = "EnterAgeButton"
+        Me.EnterAgeButton.Size = New System.Drawing.Size(103, 56)
+        Me.EnterAgeButton.TabIndex = 0
+        Me.EnterAgeButton.Text = "Submit"
+        Me.EnterAgeButton.UseVisualStyleBackColor = True
+        '
+        'AgeListBox
+        '
+        Me.AgeListBox.FormattingEnabled = True
+        Me.AgeListBox.Location = New System.Drawing.Point(167, 108)
+        Me.AgeListBox.Name = "AgeListBox"
+        Me.AgeListBox.Size = New System.Drawing.Size(246, 95)
+        Me.AgeListBox.TabIndex = 1
+        '
+        'AgeTextBox
+        '
+        Me.AgeTextBox.Location = New System.Drawing.Point(167, 53)
+        Me.AgeTextBox.Name = "AgeTextBox"
+        Me.AgeTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.AgeTextBox.TabIndex = 2
+        '
+        'EnterAgeLabel
+        '
+        Me.EnterAgeLabel.AutoSize = True
+        Me.EnterAgeLabel.Location = New System.Drawing.Point(91, 55)
+        Me.EnterAgeLabel.Name = "EnterAgeLabel"
+        Me.EnterAgeLabel.Size = New System.Drawing.Size(60, 13)
+        Me.EnterAgeLabel.TabIndex = 3
+        Me.EnterAgeLabel.Text = "Enter Age: "
+        '
+        'AgeValidatorLabel
+        '
+        Me.AgeValidatorLabel.AutoSize = True
+        Me.AgeValidatorLabel.ForeColor = System.Drawing.Color.Red
+        Me.AgeValidatorLabel.Location = New System.Drawing.Point(463, 54)
+        Me.AgeValidatorLabel.Name = "AgeValidatorLabel"
+        Me.AgeValidatorLabel.Size = New System.Drawing.Size(0, 13)
+        Me.AgeValidatorLabel.TabIndex = 4
+        Me.AgeValidatorLabel.Visible = False
+        '
+        'OldMemberButton
+        '
+        Me.OldMemberButton.Location = New System.Drawing.Point(167, 300)
+        Me.OldMemberButton.Name = "OldMemberButton"
+        Me.OldMemberButton.Size = New System.Drawing.Size(103, 56)
+        Me.OldMemberButton.TabIndex = 5
+        Me.OldMemberButton.Text = "Find Oldest Member"
+        Me.OldMemberButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(299, 299)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(103, 56)
+        Me.ExitButton.TabIndex = 6
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(672, 500)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.OldMemberButton)
+        Me.Controls.Add(Me.AgeValidatorLabel)
+        Me.Controls.Add(Me.EnterAgeLabel)
+        Me.Controls.Add(Me.AgeTextBox)
+        Me.Controls.Add(Me.AgeListBox)
+        Me.Controls.Add(Me.EnterAgeButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents EnterAgeButton As System.Windows.Forms.Button
+    Friend WithEvents AgeListBox As System.Windows.Forms.ListBox
+    Friend WithEvents AgeTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents EnterAgeLabel As System.Windows.Forms.Label
+    Friend WithEvents AgeValidatorLabel As System.Windows.Forms.Label
+    Friend WithEvents OldMemberButton As System.Windows.Forms.Button
+    Friend WithEvents ExitButton As System.Windows.Forms.Button
+
+End Class
---
+++ Quiz_Two/Quiz_Two/Form1.vb
@@ -0,0 +1,51 @@
+﻿'Your client requires an application to determine the age of the oldest person in any family.
+'Your program will use a textbox to accept ages and add the numbers to a listbox.
+'To find the oldest member of a family the user will click a button which will use a loop (see text p.295) to find the largest number in the listbox.
+'As this exercise is designed to give you practice with looping do not sort the listbox for your solution.
+
+Public Class Form1
+
+    Private AgeValidator As Boolean
+    Private OldestAge As Integer
+
+    'Adds the Age entered in the TextBox to the list box.
+    Private Sub EnterAgeButton_Click(sender As Object, e As EventArgs) Handles EnterAgeButton.Click
+        AgeValidatorLabel.Visible = False
+        ValidateAge()
+        If (AgeValidator = True) Then
+            AgeListBox.Items.Add(AgeTextBox.Text)
+        End If
+    End Sub
+
+    'Validates the users input and assigns true or false to AgeValidator boolean
+    Private Function ValidateAge()
+        If (AgeTextBox.Text.Equals("") Or IsNumeric(AgeTextBox.Text) = False) Then
+            AgeValidator = False
+            AgeValidatorLabel.Text = "Please enter a valid Age"
+            AgeValidatorLabel.Visible = True
+            Return AgeValidator
+        ElseIf (Convert.ToInt16(AgeTextBox.Text) <= 0) Then
+            AgeValidator = False
+            AgeValidatorLabel.Text = "You can't be less than 0 years dummy"
+            AgeValidatorLabel.Visible = True
+            Return AgeValidator
+        End If
+        AgeValidator = True
+        Return AgeValidator
+    End Function
+
+    'Finds the value of the oldest member of the family
+    Private Sub OldMemberButton_Click(sender As Object, e As EventArgs) Handles OldMemberButton.Click
+        For Each Age In AgeListBox.Items
+            If (Age >= OldestAge) Then
+                OldestAge = Age
+            End If
+        Next
+        MessageBox.Show("The oldest Age is: " + OldestAge.ToString())
+    End Sub
+
+    'Closes the program
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+End Class
